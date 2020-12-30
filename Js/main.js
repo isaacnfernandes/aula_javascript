c5b03631f00ff71435b7e8a1284433ca5b8dfc32
ISAAC FERNANDES <isaac.fernandes@ebdbr.com.br>
2020-12-30 18:22:40 -0300

    aula javascript

---
+++ Js/main.js
@@ -0,0 +1,95 @@
+function clicou(){
+	alert("Obrigado por clicar");
+}
+
+
+
+/*function soma(n1, n2){
+	return n1 + n2;
+}
+alert(soma(5,10));
+
+
+function setReplace(frase, nome, novo_nome){
+	return frase.replace(nome, novo_nome);
+}
+
+alert(setReplace("Vai Japão", "Japão", "Brasil"));
+
+
+var validar = 0;
+
+function validarIdade(idade){
+	if (idade >= 18) {
+		validar = true;
+	}else{
+		validar = false;
+	}
+	return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validarIdade(idade);
+console.log(validar);
+*/
+
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getMinutes());
+alert(d.getDay());
+alert(d.getHours());
+*/
+
+/*
+var count = 0;
+while (count < 5){
+	console.log(count);
+	count ++;
+}
+
+for (count = 0; count <= 5; count ++){
+	console.log(count);
+}
+*/
+
+
+/*
+var idade = prompt("Qual a sua idade?");
+
+if (idade >= 18) {
+	alert("Maior de idade.");
+}else{
+	alert("Menor de idade");
+}
+*/
+
+/*
+var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[0].nome);
+*/
+
+/*
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+alert(fruta.nome);
+*/
+
+/*
+var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+lista.pop()
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+alert(lista[1]);
+*/
+
+/*
+var nome = "Isaac Fernandes";
+alert("meu primeiro JS" + " " +nome);
+var frase = "Japão é o melhor time do mundo";
+console.log(frase.toLowerCase());
+alert(frase.replace("Japão", "Brasil"));
+*/
